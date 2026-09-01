ac27f9dc9a21d029333dc0aa91cbce159b494d11
JesterRomut <lxcmoriah@gmail.com>
2026-09-01 19:46:22 +0800

    初始化仓库

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,47 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+// @ts-nocheck
+import starlight from "@astrojs/starlight";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					items: [{ autogenerate: { directory: 'reference' } }],
-				},
-			],
-		}),
-	],
+  i18n: {
+    locales: [
+      {
+        path: "cn", // no slashes included
+        codes: ["zh-CN", "zh-TW"],
+      },
+      "en",
+    ],
+    defaultLocale: "cn",
+    routing: {
+      prefixDefaultLocale: true,
+    },
+  },
+  markdown: {
+    syntaxHighlight: false,
+  },
+  integrations: [
+    starlight({
+      title: "魂之挽歌 Wiki",
+      logo: {
+        src: "./src/assets/icon.png",
+      },
+      customCss: ["./src/styles/main.css"],
+      expressiveCode: false,
+      favicon: "/favicon.ico",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/withastro/starlight",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Reference",
+          items: [{ autogenerate: { directory: "reference" } }],
+        },
+      ],
+    }),
+  ],
 });
